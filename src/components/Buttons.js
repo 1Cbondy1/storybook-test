b665885714662de0dad28493754a42d954da77a8
Clay <claytonbondy@gmail.com>
2019-10-15 10:24:02 -0500

    heirarchy

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../App.css'
 
-export default function Button() {
+export default function Buttons() {
   return (
       <div>
         <button className="button">Primary</button>
+        <button className="button-secondary">Secondary</button>
       </div>
   );
 }
--- src/stories/Buttons.stories.js
+++ src/stories/Buttons.stories.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import Button from '../components/Button';
+import Buttons from '../components/Buttons';
 
-storiesOf('Button', module)
+storiesOf('Buttons', module)
   .add(
     'default',
     () => (
-      <Button/>
+      <Buttons/>
     ),
     {
       'in-dsm': {
